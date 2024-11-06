e471cebffe2a3735bc0807bc5c01fcefd8f041b1
matteuo <contact@matteuo.dev>
2024-11-06 16:15:26 +0100

    edit test app

--- projects/test-app/src/app/app.module.ts
+++ projects/test-app/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { StepJourneyModule } from 'step-journey';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StepJourneyModule  // Aggiungi la tua libreria qui
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
