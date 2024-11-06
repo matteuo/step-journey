da77c7b71d284aa229a792ea7933d3187137f958
matteuo <contact@matteuo.dev>
2024-11-06 17:38:52 +0100

    test update steps

--- projects/test-app/src/app/app.component.ts
+++ projects/test-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StepJourneyComponent } from 'step-journey';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-app';
+
+  // Definisci la lista di steps iniziale
+  steps: StepJourneyComponent["steps"] = [
+    { label: 'Step 1', status: 'completed', color: '#4CAF50' },
+    { label: 'Step 2', status: 'current', color: '#FF9800' },
+    { label: 'Step 3', status: 'pending'},
+    { label: 'Step 4', status: 'pending' }
+  ];
+
+  // Metodo per aggiornare la lista di steps
+  updateSteps() {
+    // Aggiorna con un nuovo riferimento per forzare l'aggiornamento del template
+    this.steps = [
+      { label: 'Step 1', status: 'completed', color: '#4CAF50' },
+      { label: 'Step 2', status: 'completed' },
+      { label: 'Step 3', status: 'current', color: '#FF9800' },
+      { label: 'Step 4', status: 'pending' }
+    ];
+  }
 }
