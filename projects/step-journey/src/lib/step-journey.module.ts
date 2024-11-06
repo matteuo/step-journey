0539452658e4be87b623559b6b23ee7b8e0fbee4
matteuo <contact@matteuo.dev>
2024-11-06 15:48:52 +0100

    add components

--- projects/step-journey/src/lib/step-journey.module.ts
+++ projects/step-journey/src/lib/step-journey.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { StepJourneyComponent } from './step-journey.component';
+import { StepComponent } from './step/step.component';
+import { DividerComponent } from './divider/divider.component';
 
 
 
 @NgModule({
   declarations: [
-    StepJourneyComponent
+    StepJourneyComponent,
+    StepComponent,
+    DividerComponent
   ],
   imports: [
   ],
---
+++ projects/step-journey/src/lib/step/step.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-step',
+  templateUrl: './step.component.html',
+  styleUrls: ['./step.component.css']
+})
+export class StepComponent {
+
+}
