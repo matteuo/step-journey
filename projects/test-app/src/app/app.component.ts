9c0829b5f92fa907e874e0f8c476ab9e9811dd55
matteuo <contact@matteuo.dev>
2024-11-11 11:19:02 +0100

    add test component max width

--- projects/test-app/src/app/app.component.ts
+++ projects/test-app/src/app/app.component.ts
@@ -9,6 +9,8 @@ import { StepJourneyComponent } from 'step-journey';
 export class AppComponent {
   title = 'test-app';
 
+  maxWidth: string = "600px";
+
   // Define the list of starting steps
   steps: StepJourneyComponent["steps"] = [
     { label: 'Step 1', status: 'completed', color: '#4CAF50' },
@@ -19,11 +21,18 @@ export class AppComponent {
 
   // Update the list of steps
   updateSteps() {
+
+    this.maxWidth = "1200px";
+
     this.steps = [
       { label: 'Step 1', status: 'completed', color: '#4CAF50' },
       { label: 'Step 2', status: 'completed' },
       { label: 'Step 3', status: 'current', color: '#FF9800' },
-      { label: 'Step 4', status: 'pending' }
+      { label: 'Step 4', status: 'pending' },
+      { label: 'Step 5', status: 'pending' },
+      { label: 'Step 6', status: 'pending' },
+      { label: 'Step 7', status: 'pending' },
+      { label: 'Step 8', status: 'pending' }
     ];
   }
 }
