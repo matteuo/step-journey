4e6397e4583fa7b5046c9d3f4e752e918f1962cc
matteuo <contact@matteuo.dev>
2024-11-11 12:37:24 +0100

    change test app

--- projects/test-app/src/app/app.component.ts
+++ projects/test-app/src/app/app.component.ts
@@ -9,8 +9,6 @@ import { StepJourneyComponent } from 'step-journey';
 export class AppComponent {
   title = 'test-app';
 
-  maxWidth: string = "600px";
-
   // Define the list of starting steps
   steps: StepJourneyComponent["steps"] = [
     { label: 'Step 1', status: 'completed', color: '#4CAF50' },
@@ -21,18 +19,13 @@ export class AppComponent {
 
   // Update the list of steps
   updateSteps() {
-
-    this.maxWidth = "1200px";
-
     this.steps = [
       { label: 'Step 1', status: 'completed', color: '#4CAF50' },
       { label: 'Step 2', status: 'completed' },
-      { label: 'Step 3', status: 'current', color: '#FF9800' },
-      { label: 'Step 4', status: 'pending' },
-      { label: 'Step 5', status: 'pending' },
-      { label: 'Step 6', status: 'pending' },
-      { label: 'Step 7', status: 'pending' },
-      { label: 'Step 8', status: 'pending' }
+      { label: 'Step 3', status: 'completed' },
+      { label: 'Step 4', status: 'current', color: '#FF9800' },
+      { label: 'Step 5', status: 'pending' }
     ];
   }
+
 }
